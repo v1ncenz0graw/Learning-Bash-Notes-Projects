936381e0880a2372280bb01ba1d39af532eee044
Vincenzo Graw <kaydzyu1508@gmail.com>
2024-11-28 09:59:42 +0500

    Task Complete!!!

--- main.sh
+++ main.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env bash
 
+# Creates new text file
+touch output.txt
+
+# Writes current date to text file
+DATE=$(date +%d/%m/%Y)
+echo "Today: ${DATE}" >output.txt
+
+# Writes Fullname to text file
+echo "Fullname: $1 $2" >>output.txt
+
+# Creates backup for text file
+cp output.txt output_backup.txt
+
+# Prints content of text file
+cat <output.txt
